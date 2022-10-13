7ce38c10a709c8a8c1c9448e3b23239fb809e96f
EkaIasaghashvili <e.iasaghashvili@gmail.com>
2022-10-13 14:22:57 +0400

    add HomeWork5 -Task1, Task2, Task3, Task4, Task5

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -6,9 +6,101 @@ namespace HomeWork4
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ReadLine();
-            Console.WriteLine("Ekatrine Iasaghashvili");
+            #region Task1
+            //Console.WriteLine("Enter the integer nummber : ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if (number%5 == 0)
+            //{
+            //    Console.WriteLine($"Input : {number} Output : Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Input : {number} Output : No");
+            //}
+            #endregion
+
+            #region Task2
+            //Console.WriteLine("Enter the integer nummber one : ");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the integer nummber two : ");
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
+            //int addition = 0;
+            //int subtraction = 0;
+            //int multiplication = 0;
+            //int division = 0;
+
+            //int min = 0;
+            //int max = 0;
+
+            //if (firstNumber < secondNumber)
+            //{
+            //    min = firstNumber;
+            //    max = secondNumber;
+            //}
+            //else
+            //{
+            //    min = secondNumber;
+            //    max = firstNumber;
+            //}
+
+            //addition = max + min;
+            //Console.WriteLine($"{min} + {max}    =    {addition}");
+
+            //subtraction = max - min;
+            //Console.WriteLine($"{max} - {min}    =    {subtraction}");
+
+            //multiplication = max * min;
+            //Console.WriteLine($"{min} * {max}    =    {multiplication}");
+
+            //if (min != 0)
+            //{
+            //    division = max / min;
+            //    Console.WriteLine($"{max} / {min}    =    {division}");
+            //}
+            //else
+            //    Console.WriteLine("Not Allowed To Divide By Zero, please enter positive number");
+
+
+            #endregion
+
+            #region Task3
+            //Console.WriteLine("Enter the integer nummber one : ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the integer nummber two : ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Input : a = {a} ; b = {b} ");
+
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            //Console.WriteLine($"Input : a = {a} ; b = {b} ");
+            #endregion
+
+            #region Task4
+            //Console.WriteLine("Enter the integer nummber : ");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //int result = 0;
+            //Console.WriteLine("Output");
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    result = userInput * i;
+            //    Console.WriteLine($"{userInput} * {i} = {result}");
+            //}
+
+            #endregion
+
+            #region Task5
+            //Console.WriteLine("Enter the integer nummber : ");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Output");
+            //for (int i = 1; i < userInput; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i*i);
+
+            //}
+
+            #endregion
         }
     }
 }
