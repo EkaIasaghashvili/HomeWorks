88daba79ff70e7520814b7e28b23b610c4ddf9fe
EkaIasaghashvili <e.iasaghashvili@gmail.com>
2022-11-11 17:45:11 +0400

    Add HomeWork13 - Taks1, Task2

---
+++ HomeWork13/Middleware/BookingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork13.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace HomeWork13.Middleware
+{
+    public class BookingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BookingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext, IConfiguration config)
+        {
+            var status = Convert.ToBoolean(config.GetSection("BookingNotAllowed").Value);
+            if(!status)
+                return _next(httpContext);
+            else
+                return httpContext.Response.WriteAsync("Booking Not Allowed");
+        }
+    }
+}
